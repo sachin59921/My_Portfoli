ab2a1ee06c423cfd02fc7fbf06944028203910f4
SACHIN KUMAR PAL <101247442+sachin59921@users.noreply.github.com>
2025-06-01 15:42:57 +0530

    updated

--- src/components/Aboutpage/Aboutpage.js
+++ src/components/Aboutpage/Aboutpage.js
@@ -16,24 +16,38 @@ function Aboutpage() {
               Software Engineer based in Ponda, Goa. I'm currently pursuing my
               Master of Computer Applications (MCA) at Goa University and
               actively seeking entry-level or internship opportunities in
-              Software Quality Assurance (QA).<br> 🛠️ I recently completed a QA
+              Software Quality Assurance (QA).
+              
+              <br> 🛠️ I recently completed a QA
               internship at SJ Innovation Pvt. Ltd., where I gained hands-on
-              experience in:</br> Manual testing of web applications Designing and
+              experience in:</br> 
+
+              <br>Manual testing of web applications Designing and
               executing detailed test cases Bug tracking and reporting using
               Jira Working in Agile teams, participating in daily stand-ups and
               sprint planning Performing regression and functional testing to
               ensure smooth, high-quality releases This experience helped me
               build a solid foundation in QA processes, SDLC, and team
-              collaboration — and fueled my passion for delivering reliable and
-              user-focused products. <br>💻 I also bring frontend skills in HTML5,
+              collaboration</br> 
+              
+              <br> — and fueled my passion for delivering reliable and
+              user-focused products.</br> 
+              
+              <br>💻 I also bring frontend skills in HTML5,
               CSS3, JavaScript, and React.js, which help me understand user
-              interfaces from both a developer and a tester’s perspective —
-              making my QA work more insightful and efficient.</br> <br>📸 Outside of
-              tech, I enjoy photography and exploring new places, which enhance
-              my attention to detail and creative thinking.</br> <br> 🚀 I’m currently
+              interfaces from both a developer and a tester’s perspective <br>
+
+              <br>— making my QA work more insightful and efficient.</br>
+              
+              <br>📸 Outside of tech, I enjoy photography and exploring new places, which enhance
+              my attention to detail and creative thinking.</br> 
+              
+              <br> 🚀 I’m currently
               open to work and looking for roles such as QA Intern, Junior QA
               Engineer, or Manual Tester, where I can grow under mentorship and
-              contribute to high-quality software development.</br> <br>📬 Let’s connect!
+              contribute to high-quality software development.</br> 
+              
+              <br>📬 Let’s connect!
               Feel free to reach out if you’re hiring, collaborating, or want to
               explore how we can work together.</br>
             </p>
